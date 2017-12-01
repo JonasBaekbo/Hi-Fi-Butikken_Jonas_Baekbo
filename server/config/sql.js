badd4c75054944d2f637f486fd6ccc5dbd0c7163
Jonas Bækbo <31005038+JonasBaekbo@users.noreply.github.com>
2017-12-01 09:42:43 +0100

    Ny SQL med serverimplementering

--- server/config/sql.js
+++ server/config/sql.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
 
 module.exports = {
-   'connect': () => {
-      return mysql.createConnection({
-         'host': 'localhost',
-         'user': 'root',
-         'password': '',
-         'database': 'hifi_butik'
-      });
-   }
+    'connect': () => {
+        return mysql.createConnection({
+            'host': '128.199.57.136',
+            'user': 'root',
+            'password': '1234',
+            'database': 'hifi_butik'
+        });
+    }
 };
